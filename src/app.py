7c5638559dc9950451517e3ab570e44950b7572f
Juan Lizondo <j.lizondolancharro@gmail.com>
2023-11-29 16:31:45 +0000

    finished

--- src/app.py
+++ src/app.py
@@ -1,15 +1,46 @@
-import os
-from sqlalchemy import create_engine
-import pandas as pd
+#Import necessary librarys
 from dotenv import load_dotenv
-
-# load the .env file variables
+import os
+import pandas as pd
 load_dotenv()
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+#Connect with API SPOTIFY
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+#Into the SPOTIPY
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+#Select my artist
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id = client_id, client_secret=client_secret))
+band = 'spotify:artist:1vBn5Puz4mdZopZEHq1QDq'
+artist = sp.artist(band)
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+#Select your Top Tracks
+top_tracks = sp.artist_top_tracks(band)
+track_data = []
+for track in top_tracks['tracks']:
+    track_ = track['name']
+    popularity = track['popularity']
+    duration = track['duration_ms']/60000
+    track_data.append({"Track": track_, "Popularity" : popularity, "Duration" : duration})
+
+#Create a DataFrame       
+the_moda = pd.DataFrame(track_data)
+
+#Show top 3 songs by popularity
+print(the_moda[:3])
+
+#Create un scatter plot to release Pupularity and Duration on my artist
+import matplotlib.pyplot as plt
+
+plt.figure(figsize = (10, 5))
+
+plt.scatter(the_moda["Popularity"],the_moda["Duration"], label = "Popularity&Duration")
+
+plt.title(artist['name'])
+plt.legend()
+plt.show()
+
+print("After seeing the graph, we can see how in my case, popularity has no relationship with the duration of the M.O.D.A songs.")
